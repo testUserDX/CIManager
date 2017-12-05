0428b1679358155a0ddf179161d93363e019a30d
vadim <vadim.reznichenko@ctdev.io>
2017-12-05 11:34:09 +0200

    add user credential object and modify pages controllers

--- src/main/java/com/web/controller/WelcomeController.java
+++ src/main/java/com/web/controller/WelcomeController.java
@@ -1,25 +1,35 @@
 package com.web.controller;
 
 
+import com.data.UserCredentials;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import java.util.Date;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-@RequestMapping({"/loginpage","/" })
 public class WelcomeController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView showMenu() {
-        return new ModelAndView("loginpage");
+
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    public String redirectToLogin() {
+        return "redirect:/loginpage";
     }
 
-    @RequestMapping("/homepage")
-    public String showHomepage(){
-        return "homepage";
+    @RequestMapping(value = {"/loginpage"}, method = RequestMethod.GET)
+    public ModelAndView showMenu() {
+        return new ModelAndView("loginpage", "credentials", new UserCredentials());
     }
+
+
+    @RequestMapping(value = "/loginpage", method = RequestMethod.POST)
+    public String verifyUser(@ModelAttribute UserCredentials userCredentials ) {
+        System.out.println("------------------------------- "+ userCredentials.getEmail()+" -----------------------");
+       //TODO logic user verefication
+        return "redirect:/homepage";
+    }
+
 }
 
 

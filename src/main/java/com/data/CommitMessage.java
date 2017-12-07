db65dfb5d7914440920e9334d4640c196081945f
Andrii <andretsykh1324@gmail.com>
2017-12-07 17:01:47 +0200

    Edit CommitPageController and commitpage, add CommitMessage object

---
+++ src/main/java/com/data/CommitMessage.java
@@ -0,0 +1,14 @@
+package com.data;
+
+public class CommitMessage {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/web/controller/CommitPageController.java
+++ src/main/java/com/web/controller/CommitPageController.java
@@ -1,13 +1,27 @@
 package com.web.controller;
 
+import com.data.CommitMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CommitPageController {
 
-    @RequestMapping("/commitpage")
-    public String commit(){
+    @RequestMapping(value = "/commitpage", method = RequestMethod.GET)
+    public String commit(Model model){
+        model.addAttribute("cmessage",new CommitMessage());
         return "commitpage";
+    }
+
+    @RequestMapping(value = "/commitpage", method = RequestMethod.POST)
+    public String getMessage(@ModelAttribute("cmessage") CommitMessage message){
+        System.out.println(message.getMessage());
+        return "redirect: /commitpage";
     }
 }

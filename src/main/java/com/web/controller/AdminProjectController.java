d5185c4bdc8a3cd1d78f0eaae498dab7d8d2fc3d
Andrii <andretsykh1324@gmail.com>
2017-12-18 15:24:11 +0200

    Made delete org method and link

--- src/main/java/com/web/controller/AdminProjectController.java
+++ src/main/java/com/web/controller/AdminProjectController.java
@@ -10,11 +10,9 @@ import com.service.daoService.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.jws.WebParam;
 import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.List;
@@ -34,31 +32,33 @@ public class AdminProjectController {
 
 
     @RequestMapping(value = "/projectmanagepage" , method = RequestMethod.GET)
-    public String showData(@RequestParam(value = "projid" ,required = false) Long projid, Model model, HttpSession session){
+    public String showData(@RequestParam(value = "projid" ,required = false) Long projid,
+                           Model model, HttpSession session){
 
-        session.setAttribute("projid",projid);
+            session.setAttribute("projid",projid);
 
-        Project project = projectDao.findFullProject(projid);
-        model.addAttribute("project",project);
+            Project project = projectDao.findFullProject(projid);
+            model.addAttribute("project",project);
 
-        List<Org> orgs = project.getOrgList();
-        model.addAttribute("projectOrgs",orgs);
+            List<Org> orgs = project.getOrgList();
 
-        System.out.println(project.getOrgList().size() +"==============================================");
-        System.out.println(project.getOrgList().get(0).getUserList().size() +"==============================================");
+            model.addAttribute("projectOrgs",orgs);
+
+            System.out.println(project.getOrgList().size() +"==============================================");
+//            if(project.getOrgList())
+//            System.out.println(project.getOrgList().get(0).getUserList().size() +"==============================================");
 
 
-        List<User> users = projectDao.usersProjectListByProjectName(project.getName());
-        model.addAttribute("projectUsers",users);
-        model.addAttribute("org",new Org());
+            List<User> users = projectDao.usersProjectListByProjectName(project.getName());
+            model.addAttribute("projectUsers",users);
+            model.addAttribute("org",new Org());
 
         return "projectmanagepage";
     }
 
+
     @RequestMapping(value = "/projectmanagepage",method = RequestMethod.POST)
     public String createOrg(@ModelAttribute("org") Org org, HttpSession session){
-
-
 
         org.setProjectId(projectDao.find((Long)session.getAttribute("projid")));
         org.setUserList(Arrays.asList(userDao.getUserByEmil((String)session.getAttribute("userEmail"))));
@@ -67,4 +67,14 @@ public class AdminProjectController {
 
 
     }
+
+    @RequestMapping(value="/projectmanagepage/{orgid}",method = RequestMethod.GET)
+    public String deleteOrg(@PathVariable("orgid") Long orgid,HttpSession session){
+        Project project = projectDao.findFullProject((Long)session.getAttribute("projid"));
+        List<Org> orgs = project.getOrgList();
+        orgDao.remove(orgDao.find(orgid));
+        return "redirect: /projectmanagepage?projid="+session.getAttribute("projid");
+    }
+
+
 }

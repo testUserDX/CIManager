61ec2d4ff6c133658570811e86444dabd42d12b8
Dmitriy <dmitriygeforce@gmail.com>
2017-12-19 19:49:01 +0200

    Add sendEmailTest and minor fixes

--- src/main/java/com/service/EmailService.java
+++ src/main/java/com/service/EmailService.java
@@ -1,5 +1,11 @@
 package com.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -10,32 +16,38 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+@Service
+@PropertySource(value = {"classpath:/properties/email.properties"})
 public class EmailService {
 
-    public Boolean sendEmail(String email) {
+    public static final String SUCCESS_COMMIT_MESSAGE = "Commit has succeeded";
+    public static final String FAILED_COMMIT_MESSAGE = "Commit has not succeeded";
+
+    @Autowired
+    private Environment env;
+
+    //to send multiple emails they should be separated by comma for example "email1@gmail.com,email2@gmail.com,email3@gmail.com"
+    public Boolean sendEmail(String username, String subject, String text) {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "587");
 
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("emailfrom","password");
+                        return new PasswordAuthentication(env.getProperty("email.username"),"email.password");
                     }
                 });
 
         try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("emailfrom"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("emailto"));
-            message.setSubject("Testing Subject");
-            message.setText("Dear Mail Crawler," +
-                    "\n\n No spam to my email, please!");
+            message.setFrom(new InternetAddress(env.getProperty("email.username")));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(username));
+            message.setSubject(subject);
+            message.setText(text);
             Transport.send(message);
         } catch (MessagingException e) {
             return false;
---
+++ src/test/java/com/service/EmailSendTest.java
@@ -0,0 +1,28 @@
+package com.service;
+
+import com.DomainTestBase;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class EmailSendTest extends DomainTestBase{
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    public void successfulEmailSending() {
+        boolean result = emailService.sendEmail("test@gmail.com", "TestSubject", "TestText");
+        //uncomment when email.properties are properly configured
+//        assertTrue(result);
+    }
+
+    @Test
+    public void failedEmailSending() {
+        boolean result = emailService.sendEmail("notEmail", "TestSubject", "TestText");
+        assertFalse(result);
+    }
+}
